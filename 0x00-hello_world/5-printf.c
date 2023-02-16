d34198b2195df6c3c4e031f4f65e493f8fa034ec
Enock <enockzy54@gmail.com>
2023-02-16 14:30:00 -0800

    print f function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,10 @@
-
+#include <stdio.h>
+/**
+ * main - A prgram that prints using printf
+ * Return: 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
