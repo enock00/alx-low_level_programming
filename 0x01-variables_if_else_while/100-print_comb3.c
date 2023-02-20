221aaa50b62dc0cfd369f6bba571044b1a7bcb0b
Enock <enockzy54@gmail.com>
2023-02-20 12:17:25 -0800

    different combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: "print combination of two digits"
+ * Return: Always 0
+ */
+int main(void)
+{
+	int d1, d2;
+
+	for ((d1 = 0); (d1 < 9); d1++)
+	{
+		for ((d2 = d1 + 1); (d2 < 10); d2++)
+		{
+			putchar((d1 + 10) + '0');
+			putchar((d2 + 10) + '0');
+
+			if (d1 != 8 && d2 != 9)
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+		putchar('\n');
+
+	return (0);
+}
