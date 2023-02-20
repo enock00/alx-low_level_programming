c192fe52c457788e9ede8764fd0418ed6d4f1f76
Enock <enockzy54@gmail.com>
2023-02-20 07:49:13 -0800

    print single digit followed by a line

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Entry Point
- * Description: "prints all single digit, followed by a new line"
+ * Description: "prints single digit, followed by a new line"
  * Return: Always 0
  */
 int main(void)
