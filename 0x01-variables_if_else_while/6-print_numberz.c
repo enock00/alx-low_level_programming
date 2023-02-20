56d191705a6841e9ae231d1315a3154043053e10
Enock <enockzy54@gmail.com>
2023-02-20 07:56:30 -0800

    print numbers followed by a line

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,10 +8,14 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n = 10; n++)
+	for ((n = 0); (n = 10); n++)
+	{
 		putchar((n % 10) + '0');
+	}
+	{
 
-	putchar('\n');
+		putchar('\n');
+	}
 
 	return (0);
 }
