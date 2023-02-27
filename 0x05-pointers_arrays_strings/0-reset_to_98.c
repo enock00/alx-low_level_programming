b2750533930781fce6f9d915414d073e1ad588c8
Enock <enockzy54@gmail.com>
2023-02-27 10:36:01 -0800

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 /**
  *reset_to_98 - a function that takes a pointer to an
  *int as a parameter
- *@n - integer input pointer
+ *@n: integer input pointer
  *Return: 0
  */
 void reset_to_98(int *n)
