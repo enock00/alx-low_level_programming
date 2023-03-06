894eedb2559b3bec7d6438cf0dd605c59df29bc8
Enock <enockzy54@gmail.com>
2023-03-06 09:09:55 -0800

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+  * _strpbrk - search a string for any of a set of bytes
+  * @s: source string
+  * @accept: accepted characters
+  *
+  * Return: always 0
+  */
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0, j;
+
+	while (s[i])
+	{
+		j = 0;
+
+		while (accept[j])
+		{
+			if (s[i] == accept[j])
+			{
+				s += i;
+				return (s);
+			}
+
+			j++;
+		}
+
+		i++;
+	}
+
+	return ('\0');
+}
