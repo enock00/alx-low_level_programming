2c28e0e5513a8745a1e7af04deae329c0a8f20e6
Enock <enockzy54@gmail.com>
2023-03-11 23:15:10 -0800

    libmy.a

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,12 +1,16 @@
 #include "main.h"
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ *_puts - a function that prints a string,
+ * followed by a new line, to stdout
+ *@str:string to be printed
+ *Return: 0
  */
-int main(void)
+void _puts(char *str)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+while (*str != '\0')
+{
+putchar(*str++);
+}
+putchar('\n');
 }
