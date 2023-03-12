75b8ea500791eb5ad740293aabc03bf4be45f6f1
Enock <enockzy54@gmail.com>
2023-03-12 08:08:32 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_putchar - writes character c
+ *@c: Character to print
+ *
+ *Return: On success 1,
+ * On error, -1
+ *
+ */
+int _putchar(char c)
+{
+	return (1, &c, 1);
+}
