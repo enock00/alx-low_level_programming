34939400d84463701037e7e4a59b4a0e8caadf47
Enock <enockzy54@gmail.com>
2023-03-16 04:38:04 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-main.c
+++ 0x0C-more_malloc_free/0-main.c
@@ -10,19 +10,6 @@
  */
 int main(void)
 {
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-
 char *c;
 int *i;
 float *f;
