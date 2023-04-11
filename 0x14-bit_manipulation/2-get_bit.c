7fe510cee6f5d33597a06c9cc4257937b66610a0
Enock <enockzy54@gmail.com>
2023-04-11 06:36:45 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,10 +10,14 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+
 	if (index >= sizeof(unsigned long int) * 8)
 	{
-		return -1;
+		return (-1);
 	}
-	unsigned long int mask = 1UL << index;
-	return (n & mask) ? 1 : 0;
+
+	if ((n & (1 << index)) == 0)
+		return (0);
+
+	return (1);
 }
